42fe5400f56ef2870b231b08ed7804891e13acbb
nonoro <101974448+nonoro@users.noreply.github.com>
2022-07-21 22:06:09 +0900

    Update ScannerExam.java

--- ex0721/ScannerExam.java
+++ ex0721/ScannerExam.java
@@ -1,3 +1,5 @@
+package ex0721;
+
 import java.util.Scanner;
 
 class ScannerExam {
@@ -10,10 +12,28 @@ class ScannerExam {
 
         while(run) {
             System.out.println("---------------------------------");
-            System.out.println("1. ");
+            System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
             System.out.println("---------------------------------");
-            System.out.println("---------------------------------")
-
+            System.out.print("선택> ");
+            int selectNumber = Integer.parseInt(scanner.nextLine());
+            if (selectNumber == 1) {
+                System.out.print("예금액> ");
+                int credit = Integer.parseInt(scanner.nextLine());
+                balance += credit;
+            }
+            if (selectNumber == 2) {
+                System.out.print("출금액> ");
+                int withdrawalAmount = Integer.parseInt(scanner.nextLine());
+                balance -= withdrawalAmount;
+            }
+            if (selectNumber == 3) {
+                System.out.println("잔고> " + balance);
+            }
+            if (selectNumber == 4) {
+                System.out.println();
+                run = false;
+            }
         }
+        System.out.println("프로그램 종료");
     }
 }
